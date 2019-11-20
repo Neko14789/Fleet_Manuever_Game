26c298c3c1fcc01766d7fe9b0c2a2be14866e6f9
Nico14789 <tekkitnick@gmail.com>
2019-11-20 12:43:40 +0100

    updated Make Python to exe script

--- Make_Python_to_exe.bat
+++ Make_Python_to_exe.bat
@@ -1,2 +1,2 @@
 @echo off
-pyinstaller --onefile --icon=Fleet_Manuever_Game.ico C:\Users\NAHuebsch\PycharmProjects\Fleet_Manuever_Game\Fleet_Manuever_Game.py  
+pyinstaller --onefile --icon=Fleet_Manuever_Game.ico Fleet_Manuever_Game.py  
