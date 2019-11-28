d5f8b8fb76661ebc40dc8186ccaa3f9f2f6cd256
Nico14789 <tekkitnick@gmail.com>
2019-11-28 08:39:29 +0100

    Fixed Application to be always in the Middle (Refactored broken Code) Cleaned Code, Removed unused Vars

--- View_Master_UI.py
+++ View_Master_UI.py
@@ -5,60 +5,46 @@ class View:
 
     def __init__(self):
 
-        Master = Tk()
+        master = Tk()
 
-        Master.title("Schiffe Versenken")
-        #   Master.resizable(False,False)
-        Grid.rowconfigure(Master, 1, weight=1)
-        Grid.columnconfigure(Master, 0, weight=1)
-        Master.minsize(250, 250)
+        """Master Window configuration"""
+        master.title("Schiffe Versenken")
+        #   master.resizable(False,False)
+        Grid.rowconfigure(master,1,weight=1)
+        Grid.columnconfigure(master,0,weight=1)
 
-        # Gets the requested values of the height and width.
-        window_width = Master.winfo_reqwidth()
-        window_height = Master.winfo_reqheight()
-        print("Width", window_width, "Height", window_height)
+        master.minsize(250,250)
+        master.eval('tk::PlaceWindow . center')
+        """Master Window configuration"""
 
-        # Gets both half the screen width/height and window width/height
-        position_right = int(Master.winfo_screenwidth() / 2 - window_width / 2)
-        position_down = int(Master.winfo_screenheight() / 3 - window_height / 2)
-
-        # Positions the window in the center of the page.
-        Master.geometry("+{}+{}".format(position_right,position_down))
-
-        root = Canvas()
-        root.grid(row=1, column=0, sticky=N + S + E + W)
+        """Button_Panel configuration"""
+        button_frame = LabelFrame(text="Playfield of Player 1",bg="#99ccff")
+        button_frame.grid(row=1, column=0, sticky=N + S + E + W)
 
 
-        def test(y, x, buttons):
-            childes = root.children
+        def button_press(y, x, buttons):
+            childes = button_frame.children
             print("hi", x, y)
             buttons[(x, y)].configure(bg="#66e0ff", state=DISABLED)
-
 
         height = 10
         width = 10
         cells = {}
-        timer = ""
         for i in range(height):  # Rows
-            Grid.columnconfigure(root, i, weight=1)
+            Grid.columnconfigure(button_frame, i, weight=1)
             for j in range(width):  # Columns
-                Grid.rowconfigure(root, j, weight=1)
-                k = (j, i)
-                b = Button(root, cursor="crosshair", text="□■~", height=2, width=5, command=lambda y=j, x=i, buttons=cells:
-                    test(y, x, buttons))
-                if timer == "":
-                    timer = 1
-                timer += 1
-                #   Type Coordinate übergeben ?
-
+                Grid.rowconfigure(button_frame, j, weight=1)
+                b = Button(button_frame, cursor="crosshair", text="□■~", width=5, height=2,
+                           command=lambda y=j, x=i, buttons=cells: button_press(y, x, buttons))
+                #   Type Coordinate.py for Button Position ?
                 b.grid(row=i, column=j, sticky=N + S + E + W)
 
                 cells[(i, j)] = b
 
-        root2 = Canvas(bg="#e6faff")
-        root2.grid(row=0, column=0, sticky=E + W)
+        top_frame = Frame(bg="#e6faff")
+        top_frame.grid(row=0, column=0, sticky=E + W)
 
-        msg = Label(root2, text="Testing my Fleet Maneuver Game!", bg="#e6faff")
+        msg = Label(top_frame, text="Testing my Fleet Maneuver Game!", bg="#e6faff")
         msg.pack()
 
-        Master.mainloop()
+        master.mainloop()
