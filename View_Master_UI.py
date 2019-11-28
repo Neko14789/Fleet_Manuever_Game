caf193e5cb28d96c5560f370878a385d3afef82c
Nico14789 <tekkitnick@gmail.com>
2019-11-28 08:47:42 +0100

    Renamed Vars

--- View_Master_UI.py
+++ View_Master_UI.py
@@ -22,24 +22,24 @@ class View:
         button_frame.grid(row=1, column=0, sticky=N + S + E + W)
 
 
-        def button_press(y, x, buttons):
+        def button_press(y, x, button_collection_set):
             childes = button_frame.children
             print("hi", x, y)
-            buttons[(x, y)].configure(bg="#66e0ff", state=DISABLED)
+            button_collection_set[(x, y)].configure(bg="#66e0ff", state=DISABLED)
 
-        height = 10
-        width = 10
-        cells = {}
-        for i in range(height):  # Rows
+        height_rows = 10
+        width_columns = 10
+        button_collection_set = {}
+        for i in range(height_rows):  # Rows
             Grid.columnconfigure(button_frame, i, weight=1)
-            for j in range(width):  # Columns
+            for j in range(width_columns):  # Columns
                 Grid.rowconfigure(button_frame, j, weight=1)
                 b = Button(button_frame, cursor="crosshair", text="□■~", width=5, height=2,
-                           command=lambda y=j, x=i, buttons=cells: button_press(y, x, buttons))
+                           command=lambda y=j, x=i, buttons=button_collection_set: button_press(y, x, buttons))
                 #   Type Coordinate.py for Button Position ?
                 b.grid(row=i, column=j, sticky=N + S + E + W)
 
-                cells[(i, j)] = b
+                button_collection_set[(i, j)] = b
 
         top_frame = Frame(bg="#e6faff")
         top_frame.grid(row=0, column=0, sticky=E + W)
