ff06b246b74342a7f64d6fc60ffd1540747ded81
Nico <tekkitnick@gmail.com>
2020-02-01 10:01:33 +0100

    BattleshipGame small update

--- Battleships Game.py
+++ Battleships Game.py
@@ -3,7 +3,7 @@
 
 class Ship:
 
-    def _init_(self, ship_type: type(str), ship_alignment: type(str), ship_position: type(tuple)):
+    def __init__(self, ship_type: type(str), ship_alignment: type(str), ship_position: type(tuple)):
         self._ship_alignment = ship_alignment
         self._ship_position = ship_position
         self._ship_type = ship_type
@@ -14,7 +14,11 @@ class Ship:
         ship_name_dict = self._get_name_dict_local_language()
 
         self._ship_type_full = ship_name_dict[ship_type][0]
-        self._ship_life_amount, self._ship_length = ship_name_dict[ship_type][1]
+        self._ship_length = ship_name_dict[ship_type][1]
+        self._ship_life_amount = self._ship_length
+
+        self._create_ship_part_coordinates(self._ship_alignment,self._ship_position)
+
 
     @staticmethod
     def _get_name_dict_local_language() -> dict:
@@ -27,26 +31,26 @@ class Ship:
         system_language = locale.getlocale()[0]
 
         ship_name_dict_en = {
-            "C": ("Carrier", 5),
-            "B": ("Battleship", 4),
-            "D": ("Destroyer", 3),
-            "S": ("Submarine", 2)
+            "c": ("Carrier", 5),
+            "b": ("Battleship", 4),
+            "d": ("Destroyer", 3),
+            "s": ("Submarine", 2)
         }
 
         # noinspection SpellCheckingInspection
         ship_name_dict_de = {
-            "C": "Träger",
-            "B": "Schlachtschiff",
-            "D": "Zerstörer",
-            "S": "U-Boot"
+            "c": "Träger",
+            "b": "Schlachtschiff",
+            "d": "Zerstörer",
+            "s": "U-Boot"
         }
         # noinspectionend SpellCheckingInspection
 
         ship_length_dict = {
-            "C": 5,  # Carrier
-            "B": 4,  # Battleship
-            "D": 3,  # Destroyer
-            "S": 2  # Submarine
+            "c": 5,  # Carrier
+            "b": 4,  # Battleship
+            "d": 3,  # Destroyer
+            "s": 2  # Submarine
         }
 
         if system_language == "de_DE":
@@ -54,13 +58,21 @@ class Ship:
         else:
             ship_name_dict = ship_name_dict_en
 
-        ship_name_dict = {"C": (ship_name_dict["C"], ship_length_dict["C"]),
-                          "B": (ship_name_dict["B"], ship_length_dict["B"]),
-                          "D": (ship_name_dict["D"], ship_length_dict["D"]),
-                          "S": (ship_name_dict["S"], ship_length_dict["S"])}
+        ship_name_dict = {"c": (ship_name_dict["c"], ship_length_dict["c"]),
+                          "b": (ship_name_dict["b"], ship_length_dict["b"]),
+                          "d": (ship_name_dict["d"], ship_length_dict["d"]),
+                          "s": (ship_name_dict["s"], ship_length_dict["s"])}
 
         return ship_name_dict
 
+    @staticmethod
+    def _create_ship_part_coordinates(ship_alignment: type(str), ship_position: type(tuple)):
+
+        ship_part_coordinates = []
+        status = "alive"
+        coordinate = (ship_position, status)
+        ship_part_coordinates.append(coordinate)
+        print("test")
 
 class Fleet:
     pass
@@ -91,7 +103,6 @@ class MasterUi(QMainWindow):
         self.initUI()
 
     def initUI(self):
-
         self.label = QtWidgets.QLabel(self)
         self.label.setText("Label")
         self.label.move(60, 60)
@@ -101,19 +112,21 @@ class MasterUi(QMainWindow):
         self.b1.move(60, 30)
         self.b1.clicked.connect(self.clicked)
 
-
     def clicked(self):
         self.label.setText("du hast den Knopf gedrückt")
         self.update()
 
-
     def update(self):
         self.label.adjustSize()
 
-def window():
-    app = QApplication(sys.argv)
-    win = MasterUi()
-    win.show()
-    sys.exit(app.exec_())
 
-window()
+# def window():
+#     app = QApplication(sys.argv)
+#     win = MasterUi()
+#     win.show()
+#     sys.exit(app.exec_())
+#
+# window()
+
+newShip = Ship("c", "h", (2, 6))
+print("test")
---
+++ test_UI.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\Users\Neko\PycharmProjects\Fleet_Manuever_Game\test_UI.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1001, 705)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.button1 = QtWidgets.QPushButton(self.centralwidget)
+        self.button1.setGeometry(QtCore.QRect(160, 280, 371, 201))
+        self.button1.setObjectName("button1")
+        self.label1 = QtWidgets.QLabel(self.centralwidget)
+        self.label1.setGeometry(QtCore.QRect(220, 140, 221, 91))
+        self.label1.setObjectName("label1")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1001, 21))
+        self.menubar.setObjectName("menubar")
+        self.menuFUck = QtWidgets.QMenu(self.menubar)
+        self.menuFUck.setObjectName("menuFUck")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.menubar.addAction(self.menuFUck.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.button1.setText(_translate("MainWindow", "Drück Mich Endlich!"))
+        self.label1.setText(_translate("MainWindow", "Hallo, mein Name ist Nico"))
+        self.menuFUck.setTitle(_translate("MainWindow", "Flieia"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
